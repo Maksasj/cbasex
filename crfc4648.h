e8cf2dea6127035f9af01916114dc4fcb8030edc
Maksim Jaroslavcevas <radioboos@gmail.com>
2024-11-06 16:06:56 +0200

    base32 hex

--- crfc4648.h
+++ crfc4648.h
@@ -17,12 +17,6 @@ uint64_t encode_base32(char** dst, void* src, uint64_t size);
 uint64_t encode_base32hex(char** dst, void* src, uint64_t size);
 uint64_t encode_base16(char** dst, void* src, uint64_t size);
 
-typedef struct {
-    unsigned char a;
-    unsigned char b;
-    unsigned char c;
-} bit_24_t;
-
 #ifdef CRFC_4648_IMPLEMENTATION
 
 uint64_t encode_base64(char** dst, void* src, uint64_t size) {
@@ -73,15 +67,18 @@ uint64_t encode_base32(char** dst, void* src, uint64_t size) {
     uint64_t padding = size % 5;
     uint64_t wrote = 0;
 
+    uint64_t a, b, c, d, e;
+    uint64_t quantum;
+
     uint64_t i = 0;
     for(; i < (size - padding); i += 5) {
-        uint64_t a = ((unsigned char*) src)[i];
-        uint64_t b = ((unsigned char*) src)[i + 1];
-        uint64_t c = ((unsigned char*) src)[i + 2];
-        uint64_t d = ((unsigned char*) src)[i + 3];
-        uint64_t e = ((unsigned char*) src)[i + 4];
-
-        uint64_t quantum = (a << 32) | (b << 24) | (c << 16) | (d << 8) | e;
+        a = ((unsigned char*) src)[i];
+        b = ((unsigned char*) src)[i + 1];
+        c = ((unsigned char*) src)[i + 2];
+        d = ((unsigned char*) src)[i + 3];
+        e = ((unsigned char*) src)[i + 4];
+        
+        quantum = (a << 32) | (b << 24) | (c << 16) | (d << 8) | e;
 
         (*dst)[wrote++] = base32_alphabet[(quantum >> 35) & BITE_MASK_5];
         (*dst)[wrote++] = base32_alphabet[(quantum >> 30) & BITE_MASK_5];
@@ -94,25 +91,55 @@ uint64_t encode_base32(char** dst, void* src, uint64_t size) {
         (*dst)[wrote++] = base32_alphabet[(quantum >> 0) & BITE_MASK_5];
     }
 
-    /*
     a = ((unsigned char*) src)[i];
     b = ((unsigned char*) src)[i + 1];
     c = ((unsigned char*) src)[i + 2];
-
-    quantum = (a << 16) | (b << 8) | c;
+    d = ((unsigned char*) src)[i + 3];
+    e = ((unsigned char*) src)[i + 4];
+        
+    quantum = (a << 32) | (b << 24) | (c << 16) | (d << 8) | e;
 
     if(padding == 1) {
-        (*dst)[wrote++] = base64_alphabet[(quantum >> 18) & 0x3F];
-        (*dst)[wrote++] = base64_alphabet[(quantum >> 12) & 0x3F];
+        (*dst)[wrote++] = base32_alphabet[(quantum >> 35) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_alphabet[(quantum >> 30) & BITE_MASK_5];
+        (*dst)[wrote++] = '=';
+        (*dst)[wrote++] = '=';
+
+        (*dst)[wrote++] = '=';
+        (*dst)[wrote++] = '=';
         (*dst)[wrote++] = '=';
         (*dst)[wrote++] = '=';
     } else if(padding == 2) {
-        (*dst)[wrote++] = base64_alphabet[(quantum >> 18) & 0x3F];
-        (*dst)[wrote++] = base64_alphabet[(quantum >> 12) & 0x3F];
-        (*dst)[wrote++] = base64_alphabet[(quantum >> 6) & 0x3F];
+        (*dst)[wrote++] = base32_alphabet[(quantum >> 35) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_alphabet[(quantum >> 30) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_alphabet[(quantum >> 25) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_alphabet[(quantum >> 20) & BITE_MASK_5];
+
+        (*dst)[wrote++] = '=';
+        (*dst)[wrote++] = '=';
+        (*dst)[wrote++] = '=';
+        (*dst)[wrote++] = '=';
+    } else if(padding == 3) {
+        (*dst)[wrote++] = base32_alphabet[(quantum >> 35) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_alphabet[(quantum >> 30) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_alphabet[(quantum >> 25) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_alphabet[(quantum >> 20) & BITE_MASK_5];
+
+        (*dst)[wrote++] = base32_alphabet[(quantum >> 15) & BITE_MASK_5];
+        (*dst)[wrote++] = '=';
+        (*dst)[wrote++] = '=';
+        (*dst)[wrote++] = '=';
+    } else if(padding == 4) {
+        (*dst)[wrote++] = base32_alphabet[(quantum >> 35) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_alphabet[(quantum >> 30) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_alphabet[(quantum >> 25) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_alphabet[(quantum >> 20) & BITE_MASK_5];
+
+        (*dst)[wrote++] = base32_alphabet[(quantum >> 15) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_alphabet[(quantum >> 10) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_alphabet[(quantum >> 5) & BITE_MASK_5];
         (*dst)[wrote++] = '=';
     }
-    */
 
     (*dst)[wrote++] = '\0';
 
@@ -120,8 +147,86 @@ uint64_t encode_base32(char** dst, void* src, uint64_t size) {
 }
 
 uint64_t encode_base32hex(char** dst, void* src, uint64_t size) {
+    uint64_t padding = size % 5;
+    uint64_t wrote = 0;
 
-    return 0;
+    uint64_t a, b, c, d, e;
+    uint64_t quantum;
+
+    uint64_t i = 0;
+    for(; i < (size - padding); i += 5) {
+        a = ((unsigned char*) src)[i];
+        b = ((unsigned char*) src)[i + 1];
+        c = ((unsigned char*) src)[i + 2];
+        d = ((unsigned char*) src)[i + 3];
+        e = ((unsigned char*) src)[i + 4];
+        
+        quantum = (a << 32) | (b << 24) | (c << 16) | (d << 8) | e;
+
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 35) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 30) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 25) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 20) & BITE_MASK_5];
+
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 15) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 10) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 5) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 0) & BITE_MASK_5];
+    }
+
+    a = ((unsigned char*) src)[i];
+    b = ((unsigned char*) src)[i + 1];
+    c = ((unsigned char*) src)[i + 2];
+    d = ((unsigned char*) src)[i + 3];
+    e = ((unsigned char*) src)[i + 4];
+        
+    quantum = (a << 32) | (b << 24) | (c << 16) | (d << 8) | e;
+
+    if(padding == 1) {
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 35) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 30) & BITE_MASK_5];
+        (*dst)[wrote++] = '=';
+        (*dst)[wrote++] = '=';
+
+        (*dst)[wrote++] = '=';
+        (*dst)[wrote++] = '=';
+        (*dst)[wrote++] = '=';
+        (*dst)[wrote++] = '=';
+    } else if(padding == 2) {
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 35) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 30) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 25) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 20) & BITE_MASK_5];
+
+        (*dst)[wrote++] = '=';
+        (*dst)[wrote++] = '=';
+        (*dst)[wrote++] = '=';
+        (*dst)[wrote++] = '=';
+    } else if(padding == 3) {
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 35) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 30) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 25) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 20) & BITE_MASK_5];
+
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 15) & BITE_MASK_5];
+        (*dst)[wrote++] = '=';
+        (*dst)[wrote++] = '=';
+        (*dst)[wrote++] = '=';
+    } else if(padding == 4) {
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 35) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 30) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 25) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 20) & BITE_MASK_5];
+
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 15) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 10) & BITE_MASK_5];
+        (*dst)[wrote++] = base32_hex_alphabet[(quantum >> 5) & BITE_MASK_5];
+        (*dst)[wrote++] = '=';
+    }
+
+    (*dst)[wrote++] = '\0';
+
+    return wrote;
 }
 
 uint64_t encode_base16(char** dst, void* src, uint64_t size) {
